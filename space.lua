21c69d662a0ad2baea657111863f059a5ab765ee
zipzep <42116722+zipzeptheyoungprogrammer@users.noreply.github.com>
2021-06-08 22:26:12 +0800

    Update space.lua

--- space.lua
+++ space.lua
@@ -9,21 +9,15 @@ end
 updateGen = true
 space = {}
 currentGen=1
+StarsDied = 0
+
+
+
 
 --Space Functions
 
-math.randomseed(os.time())
-local clock = os.clock
-function sleep(n)  -- seconds
-  local t0 = clock()
-  while clock() - t0 <= n do end
-end
---Space Generation
-updateGen = true
-space = {}
-currentGen=1
 
---Space Functions
+
 function checkType(t)
   if t == 1 then
     io.write(' ' .. ' ')
@@ -36,12 +30,33 @@ function checkLives(gen)
 
 for i=1, 10 do
 	for j=1, 10 do
-		if gen > space[i][j].l then
+  
+    deathGenDate = space[i][j].GenerationBorned + space[i][j].l
+		if  gen >= deathGenDate then
         space[i][j].t = 1
+        space[i][j].hasDied = true      
     end
 	end
 end
 
+end
+
+function dwarfStar()
+
+for i=1, 10 do
+	for j=1, 10 do
+        if space[i][j].hasDied == true then
+            starChanceOfSurviving = 0.1
+            starChance = math.random(0.01, 200)
+            if (starChance < starChanceOfSurviving) then
+                 space[i][j].t =2
+                 space[i][j].l = math.random(1, 100)
+                 space[i][j].GenerationBorned = currentGen
+                 space[i][j].hasDied = false   
+            end
+        end
+    end
+	end
 end
 
 -- l= lives, t=type(1 is void, 2 is sun)
@@ -49,7 +64,7 @@ end
 for i=1, 10 do
 	table.insert(space,  {})
 	for j=1, 10 do
-		table.insert(space[i], {l=math.random(1,100), t=math.random(1,2)})
+		table.insert(space[i], {l=math.random(1,10), t=math.random(1,2), hasDied=false, GenerationBorned= 0})
 	end
 end
 
@@ -64,12 +79,13 @@ repeat
       end
   end
 
-  print(' \nCurrent Gen: ' .. currentGen )
-  sleep(0.1)
-  os.execute("cls")
+  print(' \nCurrent Gen: ' .. currentGen)
+  sleep(0.05)
+  os.execute("clear")
   currentGen = currentGen + 1
   checkLives(currentGen)
-  
+  dwarfStar()
 until(updateGen == false)
+
 
 
